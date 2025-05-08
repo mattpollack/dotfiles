ee392ce688d5a25bac683bddc730c3bf39bcd116
Matthew Pollack <matt.pck@gmail.com>
2025-05-08 11:57:37 -0400

    Back to default harpoon list

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -404,55 +404,55 @@ local harpoon = require("harpoon")
 
 harpoon:setup()
 
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-  local file_paths = {}
-
-  for _, item in ipairs(harpoon_files.items) do
-    table.insert(file_paths, item.value)
-  end
-
-  local make_finder = function()
-    local paths = {}
-
-    for _, item in ipairs(harpoon_files.items) do
-      table.insert(paths, item.value)
-    end
-
-    return require("telescope.finders").new_table({
-      results = paths,
-    })
-  end
-
-  require("telescope.pickers")
-      .new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-          results = file_paths,
-        }),
-        previewer = false,
-        sorter = conf.generic_sorter({}),
-        layout_strategy = "center",
-        attach_mappings = function(prompt_buffer_number, map)
-          -- The keymap you need
-          map("i", "<c-d>", function()
-            local state = require("telescope.actions.state")
-            local selected_entry = state.get_selected_entry()
-            local current_picker = state.get_current_picker(prompt_buffer_number)
-
-            -- This is the line you need to remove the entry
-            harpoon:list():remove(selected_entry)
-            current_picker:refresh(make_finder())
-          end)
-
-          return true
-        end,
-      })
-      :find()
-end
+-- local conf = require("telescope.config").values
+-- local function toggle_telescope(harpoon_files)
+--   local file_paths = {}
+--
+--   for _, item in ipairs(harpoon_files.items) do
+--     table.insert(file_paths, item.value)
+--   end
+--
+--   local make_finder = function()
+--     local paths = {}
+--
+--     for _, item in ipairs(harpoon_files.items) do
+--       table.insert(paths, item.value)
+--     end
+--
+--     return require("telescope.finders").new_table({
+--       results = paths,
+--     })
+--   end
+--
+--   require("telescope.pickers")
+--       .new({}, {
+--         prompt_title = "Harpoon",
+--         finder = require("telescope.finders").new_table({
+--           results = file_paths,
+--         }),
+--         previewer = false,
+--         sorter = conf.generic_sorter({}),
+--         layout_strategy = "center",
+--         attach_mappings = function(prompt_buffer_number, map)
+--           -- The keymap you need
+--           map("i", "<c-d>", function()
+--             local state = require("telescope.actions.state")
+--             local selected_entry = state.get_selected_entry()
+--             local current_picker = state.get_current_picker(prompt_buffer_number)
+--
+--             -- This is the line you need to remove the entry
+--             harpoon:list():remove(selected_entry)
+--             current_picker:refresh(make_finder())
+--           end)
+--
+--           return true
+--         end,
+--       })
+--       :find()
+-- end
 
 vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
-vim.keymap.set("n", "<leader>hl", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
+vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open harpoon window" })
 vim.api.nvim_set_keymap('n', '<leader>hf', ':InsertHarpoonFiles<CR>', { noremap = true, silent = true })
 
 -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
