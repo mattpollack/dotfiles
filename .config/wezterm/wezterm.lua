7437acc6de6334c9e12634606d2973834f83629a
Matthew Pollack <matt.pck@gmail.com>
2025-05-26 22:11:53 -0400

    Slightly more

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.6
-config.text_background_opacity = 0.3
+config.text_background_opacity = 0.4
 config.macos_window_background_blur = 100
 
 return config
