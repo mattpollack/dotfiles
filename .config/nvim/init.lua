2ad9985eb43e798934c72eb6ebbaa6fd5eb390ef
Matthew Pollack <matt.pck@gmail.com>
2025-05-28 12:18:37 -0400

    More theme stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -181,16 +181,14 @@ require("lazy").setup({
   },
   { 'echasnovski/mini.nvim', version = '*' },
   { 'itchyny/vim-qfedit' },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+  }
 })
-
-
--- SETUP
--- Default options:
-require('kanagawa').setup({
-  transparent = true
-})
-
-vim.cmd("colorscheme kanagawa")
 
 -- AFTER REMAPS
 
@@ -577,10 +575,6 @@ vim.keymap.set("n", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, si
 vim.keymap.set("n", "gr", "<cmd>CodeCompanionChat Reject<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>af', ':InsertOpenBuffers<CR>', { noremap = true, silent = true })
 
-
--- MINI DIFF
-
-
 require('mini.diff').setup({
   view = {
     style = 'sign',
@@ -592,3 +586,73 @@ require('mini.diff').setup({
     },
   }
 })
+
+-- THEME
+-- OLD THEMES:
+-- "daschw/leaf.nvim",
+-- "shaunsingh/nord.nvim",
+
+
+-- Default options:
+
+require('kanagawa').setup({
+  transparent = true,
+  -- theme = "dark",
+  -- contrast = "low",
+  overrides = function(colors)
+    local theme = colors.theme
+    return {
+      -- TelescopeTitle = { fg = theme.ui.special, bold = true },
+      -- TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+      TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = 'none' },
+      -- TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+      TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = 'none' },
+      -- TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+      TelescopePreviewBorder = { bg = 'none', fg = theme.ui.bg_dim },
+    }
+  end,
+})
+
+vim.cmd("colorscheme kanagawa")
+vim.cmd('hi! LineNr guibg=none ctermbg=none')
+vim.cmd('hi! SignColumn guibg=none ctermbg=none')
+
+--
+-- vim.g.nord_contrast = false
+-- vim.g.nord_borders = false
+-- vim.g.nord_disable_background = true
+-- vim.g.nord_italic = false
+-- vim.g.nord_uniform_diff_background = true
+-- vim.g.nord_bold = false
+--
+-- -- Load the colorscheme
+-- require('nord').set()
+--
+-- INDENT BLANKLINE
+
+local highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
+}
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+vim.g.rainbow_delimiters = { highlight = highlight }
+require("ibl").setup { scope = { highlight = highlight } }
+
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
