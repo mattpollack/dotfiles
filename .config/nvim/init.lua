13a84a4354504325681556395ceb0b903ec6b437
Matthew Pollack <matt.pck@gmail.com>
2025-09-09 10:56:50 -0400

    set a more reasonable scrolloff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,7 +12,7 @@ vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.cursorcolumn = true
-vim.opt.scrolloff = 999
+vim.opt.scrolloff = 20
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-f>", "<C-f>zz")
