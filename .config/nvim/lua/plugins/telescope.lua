1dc4a3e21b7783ca00a8787d157edc722dfd1055
Matthew Pollack <matt.pck@gmail.com>
2025-09-25 15:55:20 -0400

    Fix up treesitter

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,41 +1,42 @@
-local function get_entry_filename(entry)
-  if entry.path or entry.filename then
-    return entry.path or entry.filename
-  elseif not entry.bufnr then
-    local value = entry.value
-
-    if not value then
-      return
-    end
-
-    if type(value) == "table" then
-      value = entry.display
-    end
-
-    local sections = vim.split(value, ":")
-
-    return sections[1]
-  end
-end
-
-local function push_selected_to_harpoon(prompt_bufnr)
-  local harpoon = require("harpoon")
-  local actions = require("telescope.actions")
-  local action_state = require("telescope.actions.state")
-  local picker = action_state.get_current_picker(prompt_bufnr)
-  local multi_selections = picker:get_multi_selection()
-
-  if #multi_selections == 0 then
-    local entry = action_state.get_selected_entry()
-    if entry then
-      harpoon:list():add(get_entry_filename(entry))
-    end
-  else
-    for _, entry in ipairs(multi_selections) do
-      harpoon:list():add(get_entry_filename(entry))
-    end
-  end
-end
+-- NOTE: Using quickfix list instead now
+-- local function get_entry_filename(entry)
+--   if entry.path or entry.filename then
+--     return entry.path or entry.filename
+--   elseif not entry.bufnr then
+--     local value = entry.value
+--
+--     if not value then
+--       return
+--     end
+--
+--     if type(value) == "table" then
+--       value = entry.display
+--     end
+--
+--     local sections = vim.split(value, ":")
+--
+--     return sections[1]
+--   end
+-- end
+--
+-- local function push_selected_to_harpoon(prompt_bufnr)
+--   local harpoon = require("harpoon")
+--   local actions = require("telescope.actions")
+--   local action_state = require("telescope.actions.state")
+--   local picker = action_state.get_current_picker(prompt_bufnr)
+--   local multi_selections = picker:get_multi_selection()
+--
+--   if #multi_selections == 0 then
+--     local entry = action_state.get_selected_entry()
+--     if entry then
+--       harpoon:list():add(get_entry_filename(entry))
+--     end
+--   else
+--     for _, entry in ipairs(multi_selections) do
+--       harpoon:list():add(get_entry_filename(entry))
+--     end
+--   end
+-- end
 
 return {
   {
@@ -69,3 +70,4 @@ return {
   },
   'catgoose/telescope-helpgrep.nvim',
 }
+
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,10 @@ return {
       local configs = require("nvim-treesitter.configs")
       configs.setup({
         ensure_installed = { "lua", "go", "gdscript", "typescript", "tsx", "javascript", "fennel" },
+        modules = {},
+        sync_install = false,
+        ignore_install = {},
+        auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
         incremental_selection = {
@@ -21,13 +25,54 @@ return {
           select = {
             enable = true,
             lookahead = true,
+            -- Available Treesitter text objects for reference:
+            -- @parameter.outer/inner - Function parameters
+            -- @function.outer/inner - Function definitions
+            -- @class.outer/inner - Classes, interfaces, types
+            -- @comment.outer/inner - Comments
+            -- @conditional.outer/inner - if/else/switch statements
+            -- @loop.outer/inner - for/while loops
+            -- @block.outer/inner - Code blocks (curly braces, etc.)
+            -- @statement.outer/inner - Individual statements
+            -- @call.outer/inner - Function calls
+            -- @string.outer/inner - String literals
+            -- @number.inner - Numeric literals
+            -- @assignment.outer/inner - Variable assignments
+            -- @return.outer/inner - Return statements
+            -- @field.outer/inner - Object fields/properties
+            -- @attribute.outer/inner - Attributes/decorators
+            -- @scopename.inner - Scope names
+            -- @text.outer/inner - Text content
+            -- @text.literal.outer/inner - String literals
+            -- @text.reference.inner - References
             keymaps = {
+              -- Parameters
               ['aa'] = '@parameter.outer',
               ['ia'] = '@parameter.inner',
+
+              -- Functions
               ['af'] = '@function.outer',
               ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
+
+              -- Comments
+              ['a/'] = '@comment.outer',
+              ['i/'] = '@comment.inner',
+
+              -- Conditionals (if/else/switch)
+              ['a?'] = '@conditional.outer',
+              ['i?'] = '@conditional.inner',
+
+              -- Loops (for/while)
+              ['al'] = '@loop.outer',
+              ['il'] = '@loop.inner',
+
+              -- Statements
+              ['as'] = '@statement.outer',
+              ['is'] = '@statement.inner',
+
+              -- Calls (function calls)
+              ['ac'] = '@call.outer',
+              ['ic'] = '@call.inner',
             }
           }
         },
@@ -37,14 +82,54 @@ return {
           goto_next_start = {
             [']m'] = '@function.outer',
             [']]'] = '@class.outer',
+            [']f'] = '@function.outer',
+            [']c'] = '@class.outer',
+            [']a'] = '@parameter.outer',
+            [']b'] = '@block.outer',
+            [']s'] = '@statement.outer',
+            [']l'] = '@loop.outer',
+            [']?'] = '@conditional.outer',
           },
           goto_next_end = {
             [']M'] = '@function.outer',
             [']['] = '@class.outer',
+            [']F'] = '@function.outer',
+            [']C'] = '@class.outer',
+            [']A'] = '@parameter.outer',
+            [']B'] = '@block.outer',
+            [']S'] = '@statement.outer',
+            [']L'] = '@loop.outer',
+            [']?'] = '@conditional.outer',
+          },
+          goto_previous_start = {
+            ['[m'] = '@function.outer',
+            ['[['] = '@class.outer',
+            ['[f'] = '@function.outer',
+            ['[c'] = '@class.outer',
+            ['[a'] = '@parameter.outer',
+            ['[b'] = '@block.outer',
+            ['[s'] = '@statement.outer',
+            ['[l'] = '@loop.outer',
+            ['[?'] = '@conditional.outer',
+          },
+          goto_previous_end = {
+            ['[M'] = '@function.outer',
+            ['[]'] = '@class.outer',
+            ['[F'] = '@function.outer',
+            ['[C'] = '@class.outer',
+            ['[A'] = '@parameter.outer',
+            ['[B'] = '@block.outer',
+            ['[S'] = '@statement.outer',
+            ['[L'] = '@loop.outer',
+            ['[?'] = '@conditional.outer',
           },
         }
       })
     end
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
   },
   { 'JoosepAlviste/nvim-ts-context-commentstring' },
   'nvim-treesitter/playground',
