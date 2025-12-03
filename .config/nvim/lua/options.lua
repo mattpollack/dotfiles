01d59b923f7b732bc606ca5aadedc2026628c8b6
Matthew Pollack <matt.pck@gmail.com>
2025-12-03 15:34:13 -0500

    Get nvim ready for when mcp is also ready

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -25,6 +25,15 @@ vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
 
+-- Start Neovim server for MCP integration
+-- Disabled to avoid conflicts with multiple Neovim instances
+-- local socket_path = '/tmp/nvim'
+-- if vim.fn.serverstart(socket_path) == '' then
+--   vim.notify('Failed to start Neovim server on ' .. socket_path, vim.log.levels.WARN)
+-- else
+--   vim.notify('Neovim server listening on ' .. socket_path, vim.log.levels.INFO)
+-- end
+
 return {
   is_work_computer = is_work_computer
 }
