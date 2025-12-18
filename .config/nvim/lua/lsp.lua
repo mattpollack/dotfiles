6fcdf82b698088f9701fd9803fb65d92c2b2d829
Matthew Pollack <matt.pck@gmail.com>
2025-12-18 09:51:37 -0500

    Move away from legacy lspconfig

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -108,18 +108,14 @@ cmp.setup.cmdline(':', {
 
 require('Comment').setup()
 require('ts_context_commentstring').setup({ enable_autocmd = false })
-
-local lspconfig = require('lspconfig')
-
--- Setup neodev for better vim API support
 require("neodev").setup({})
 
-lspconfig.lua_ls.setup({
-  on_attach = on_attach,
+-- Lua Language Server
+vim.lsp.config('lua_ls', {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml', '.git' },
   capabilities = capabilities,
-  root_dir = function(fname)
-    return lspconfig.util.find_git_ancestor(fname) or vim.fn.getcwd()
-  end,
   settings = {
     Lua = {
       diagnostics = {
@@ -132,27 +128,18 @@ lspconfig.lua_ls.setup({
   }
 })
 
-lspconfig.gdscript.setup({
+vim.lsp.config('gdscript', {
   cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
-  on_attach = on_attach,
+  filetypes = { 'gd', 'gdscript', 'gdscript3' },
+  root_markers = { 'project.godot' },
   capabilities = capabilities,
-  root_dir = lspconfig.util.root_pattern("project.godot"),
 })
 
-lspconfig.ts_ls.setup({
-  on_attach = on_attach,
+vim.lsp.config('ts_ls', {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  root_markers = { 'package.json', 'tsconfig.json', 'jsconfig.json', '.git' },
   capabilities = capabilities,
-  root_dir = function(fname)
-    -- First try to find a project root
-    local root = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git")(fname)
-    if root then
-      return root
-    end
-
-    -- If no project root found, use current working directory
-    -- This allows LSP to work outside of project directories
-    return vim.fn.getcwd()
-  end,
   settings = {
     typescript = {
       inlayHints = {
@@ -187,24 +174,13 @@ lspconfig.ts_ls.setup({
       },
     },
   },
-  filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
 })
 
--- Python LSP setup with Pyright
-lspconfig.pyright.setup({
-  on_attach = on_attach,
+vim.lsp.config('pyright', {
+  cmd = { 'pyright-langserver', '--stdio' },
+  filetypes = { "python" },
+  root_markers = { 'pyproject.toml', 'setup.py', 'requirements.txt', 'Pipfile', 'pyrightconfig.json', '.git' },
   capabilities = capabilities,
-  root_dir = function(fname)
-    -- Look for Python project indicators
-    local root = lspconfig.util.root_pattern("pyproject.toml", "setup.py", "requirements.txt", "Pipfile",
-      "pyrightconfig.json", ".git")(fname)
-    if root then
-      return root
-    end
-
-    -- If no project root found, use current working directory
-    return vim.fn.getcwd()
-  end,
   settings = {
     python = {
       analysis = {
@@ -221,16 +197,30 @@ lspconfig.pyright.setup({
       },
     },
   },
-  filetypes = { "python" },
 })
 
--- Terraform LSP setup
-lspconfig.terraformls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  root_dir = lspconfig.util.root_pattern(".terraform", ".git", "terraform.workspace"),
+vim.lsp.config('terraformls', {
+  cmd = { 'terraform-ls', 'serve' },
   filetypes = { "terraform", "hcl", "tf" },
+  root_markers = { '.terraform', '.git', 'terraform.workspace' },
+  capabilities = capabilities,
 })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local bufnr = args.buf
+
+    -- Call the on_attach function
+    on_attach(client, bufnr)
+  end,
+})
+
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('gdscript')
+vim.lsp.enable('ts_ls')
+vim.lsp.enable('pyright')
+vim.lsp.enable('terraformls')
 
 vim.diagnostic.config({
   virtual_text = true
